edab74beaa3aaf7538cd7b04a36b65bd140e9b68
Maksim Ryzhikov <rv.maksim@gmail.com>
2013-04-23 01:57:21 +0400

    Исправил ссылку на gihtub и картинку

--- docs/src/ngdoc.js
+++ docs/src/ngdoc.js
@@ -316,7 +316,7 @@ Doc.prototype = {
     dom.h(title(this.name), function() {
 
       notice('deprecated', 'Deprecated API', self.deprecated);
-      dom.tag('a', {href: 'http://github.com/angular/angular.js/edit/master/' + self.file, class: 'improve-docs btn btn-primary'}, 'Improve this doc');
+      dom.tag('a', {href: 'http://github.com/maksimr/cloudbees-test/edit/master/' + self.file, class: 'improve-docs btn btn-primary'}, 'Improve translate');
       if (self.ngdoc != 'overview') {
         dom.h('Description', self.description, dom.html);
       }
