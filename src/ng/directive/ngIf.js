fb90d32e4b7bfe301e38d647eef0e5f0bfd457ab
tamtakoe <tamtakoe@gmail.com>
2013-05-13 13:06:23 +0300

    translate(api:ngIf.js):Добавил перевод

--- src/ng/directive/ngIf.js
+++ src/ng/directive/ngIf.js
@@ -6,41 +6,38 @@
  * @restrict A
  *
  * @description
- * The `ngIf` directive removes and recreates a portion of the DOM tree (HTML)
- * conditionally based on **"falsy"** and **"truthy"** values, respectively, evaluated within
- * an {expression}. In other words, if the expression assigned to **ngIf evaluates to a false
- * value** then **the element is removed from the DOM** and **if true** then **a clone of the
- * element is reinserted into the DOM**.
- *
- * `ngIf` differs from `ngShow` and `ngHide` in that `ngIf` completely removes and recreates the
- * element in the DOM rather than changing its visibility via the `display` css property.  A common
- * case when this difference is significant is when using css selectors that rely on an element's
- * position within the DOM (HTML), such as the `:first-child` or `:last-child` pseudo-classes.
- *
- * Note that **when an element is removed using ngIf its scope is destroyed** and **a new scope
- * is created when the element is restored**.  The scope created within `ngIf` inherits from 
- * its parent scope using
+ * Директива `ngIf` удаляет и восстанавливает часть дерева DOM (HTML) в зависимости от значения условного выражения 
+ * {expression} **«true»** или **«false»**, соответственно. Другими словами, если выражение, присваиваемое `ngIf` 
+ * оценивается как ложное, то элемент удаляется из DOM и если истинное, то копия элемента повторно вставляется в DOM.
+ * 
+ * `ngIf` отличается от `ngShow` и `ngHide` тем, что `ngIf` полностью удаляет и воссоздает элемент в DOM, а не 
+ * изменяет его видимость через CSS-свойство `display`. В общем случае, разница является значительной 
+ * при использовании CSS селекторов, которые берут за основу позицию элемента в DOM (HTML), такие как
+ * псевдоклассы: `:first-child` или `:last-child`.
+ * 
+ * Обратите внимание, что, **когда элемент удаляется с помощью `ngIf`, то его область видимости уничтожается** 
+ * и **новая область видимости создается при восстановлении элемента**. Область видимости, созданная в `ngIf`
+ * наследует от своего родителя область видимости с помощью 
  * {@link https://github.com/angular/angular.js/wiki/The-Nuances-of-Scope-Prototypal-Inheritance prototypal inheritance}.
- * An important implication of this is if `ngModel` is used within `ngIf` to bind to
- * a javascript primitive defined in the parent scope. In this case any modifications made to the
- * variable within the child scope will override (hide) the value in the parent scope.
- *
- * Also, `ngIf` recreates elements using their compiled state. An example scenario of this behavior
- * is if an element's class attribute is directly modified after it's compiled, using something like 
- * jQuery's `.addClass()` method, and the element is later removed. When `ngIf` recreates the element
- * the added class will be lost because the original compiled state is used to regenerate the element.
- *
- * Additionally, you can provide animations via the ngAnimate attribute to animate the **enter**
- * and **leave** effects.
+ * Важным следствием этого является то, что если `ngModel` используется в `ngIf`, то связанный JavaScript примитив
+ * определен в родительской области. В этом случае любые изменения, внесенные в переменную в дочерней области
+ * видимости будут перекрывать (скрывать) значение в родительской области.
+ * 
+ * Кроме того, `ngIf` воссоздает элементы, используя их компилированное состояние. Примерным сценарием такого 
+ * поведения является случай когда атрибут класса элемента непосредственно изменен после своей компиляции, 
+ * используя что-то вроде JQuery-метода `.addClass()`, и позже элемент удаляется. Когда `ngIf` воссоздает элемент
+ * добавленые классы будут потеряны, поскольку для регенерации элемента используется оригинальное 
+ * скомпилированное состояние.
+ * 
+ * Кроме того, можно задать анимацию с помощью атрибута ngAnimate для анимации эффектов **enter** и **leave**.
  *
  * @animations
- * enter - happens just after the ngIf contents change and a new DOM element is created and injected into the ngIf container
- * leave - happens just before the ngIf contents are removed from the DOM
- *
+ * enter - происходит после изменения содержимого `ngIf`, создания и внедрения нового DOM-элемента в контейнер `ngIf`
+ * leave - происходит перед удалением содержимого `ngIf` из DOM
+ * 
  * @element ANY
  * @scope
- * @param {expression} ngIf If the {@link guide/expression expression} is falsy then
- *     the element is removed from the DOM tree (HTML).
+ * @param {expression} ngIf Если {@link guide/expression выражение} ложно, то элемент удаляется из дерева DOM (HTML).
  *
  * @example
    <doc:example>
